dfe03461c860b3ae0587a497e210ab23a2bfe4cc
Akshit Chaturvedi <akshitc2228@gmail.com>
2025-05-04 20:24:02 +0100

    add jump animation and jump logic

--- Assets/Scripts/PlayerAnimator.cs
+++ Assets/Scripts/PlayerAnimator.cs
@@ -6,21 +6,33 @@ public class PlayerAnimator : MonoBehaviour
 {
     [SerializeField]
     private Animator _playerAnimator;
+    private Movement _movementScript;
+
+    private Rigidbody2D _playerRb;
+
+    //boolean flagNames for animation:
+    string groundedFlag = "isGrounded";
+    string jumpingFlag = "isJumping";
+    string fallingFlag = "isFalling";
+
+    float verticalVelocity;
 
     private void Awake()
     {
         _playerAnimator = GetComponent<Animator>();
+        _movementScript = GetComponent<Movement>();
     }
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        _playerRb = GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
     void Update()
     {
+        verticalVelocity = _playerRb.velocity.y;
         SwitchAnimations();
     }
 
@@ -38,6 +50,25 @@ public class PlayerAnimator : MonoBehaviour
         }
 
         //transition to jumping
-        //a checklist for a good jump: power of jump; duration needed to reach max height; max height; force of gravity; duration needed to get back to the ground
+        //if isJumping and velocity is greater than 0
+        if(!_movementScript.isGrounded && verticalVelocity > 0.1f)
+        {
+            _playerAnimator.SetBool(groundedFlag, false);
+            _playerAnimator.SetBool(jumpingFlag, true);
+        }
+        //possible cause of concerm is that grounded flag is not used and isJumping is not used when
+        //transitioning to descent animation
+        else if(!_movementScript.isGrounded && verticalVelocity <= -0.1f)
+        {
+            _playerAnimator.SetBool(groundedFlag, false);
+            _playerAnimator.SetBool(jumpingFlag, false);
+            _playerAnimator.SetBool(fallingFlag, true);
+        }
+        else
+        {
+            _playerAnimator.SetBool(groundedFlag, true);
+            _playerAnimator.SetBool(jumpingFlag, false);
+            _playerAnimator.SetBool(fallingFlag, false);
+        }
     }
 }
