eb952685ec1e0116f28c283974713392a3d44389
Akshit Chaturvedi <akshitc2228@gmail.com>
2025-10-25 20:04:46 +0100

    add weight based pulley lift

--- Assets/Scripts/Platform behavior/CrateLift.cs
+++ Assets/Scripts/Platform behavior/CrateLift.cs
@@ -1,18 +1,99 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
 
 public class CrateLift : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    [Header("Waypoints")]
+    [SerializeField] private List<Transform> targetPoints;
+    [SerializeField] private float liftMoveTime = 0.9f;
+
+    private Vector3 velocity = Vector3.zero;
+
+    private List<KeyValuePair<Transform, float>> orderedThresholds;
+    private int globalMapIndex;
+    private int localUpdatedIndex;
+    public float NetLiftWeight { get; set; }
+
+    private void Start()
     {
-        
+        var thresholds = new Dictionary<Transform, float>();
+        float threshold = 0f;
+        thresholds.Add(transform, threshold);
+        foreach (var t in targetPoints)
+        {
+            threshold += 5f;
+            thresholds.Add(t, threshold);
+        }
+
+        orderedThresholds = thresholds.OrderBy(x => x.Value).ToList();
+        globalMapIndex = 0;
+        localUpdatedIndex = 0;
     }
 
-    // Update is called once per frame
-    void Update()
+    private void FixedUpdate()
     {
-        
+        if (orderedThresholds.Count == 0) return;
+        localUpdatedIndex = globalMapIndex;
+        Vector3 targetPos = orderedThresholds[localUpdatedIndex].Key.position;
+
+        transform.position = Vector3.SmoothDamp(
+            transform.position,
+            targetPos,
+            ref velocity,
+            liftMoveTime
+        );
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag("dungeon2_pushableCrate"))
+        {
+            //check that its a crate before adding its weight else add nothing:
+            PushableCrate collidingCrate = collision.gameObject.GetComponent<PushableCrate>();
+            if (collidingCrate != null)
+            {
+                //also, the crate is now linked to the lift so weights can be stacked:
+                collidingCrate.OnALift = true;
+                NetLiftWeight += collidingCrate.Weight;
+                CheckAndUpdateMapIndex();
+            }
+        }
+
+        collision.transform.SetParent(transform);
+    }
+
+    private void OnCollisionExit2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag("dungeon2_pushableCrate"))
+        {
+            PushableCrate collidingCrate = collision.gameObject.GetComponent<PushableCrate>();
+            if (collidingCrate != null)
+            {
+                collidingCrate.OnALift = false;
+                NetLiftWeight -= collidingCrate.Weight;
+                CheckAndUpdateMapIndex();
+            }
+        }
+
+        collision.transform.SetParent(null);
+    }
+
+    //ideally I would have wanted to call this method
+    //the same way people use a useEffect in React whenever weight changes
+    //for now all I know is to make it public
+    public void CheckAndUpdateMapIndex()
+    {
+        int newIndex = 0;
+        for (int i = 0; i < orderedThresholds.Count; i++)
+        {
+            if (NetLiftWeight >= orderedThresholds[i].Value)
+                newIndex = i;
+            else
+                break;
+        }
+        globalMapIndex = newIndex;
     }
 }
+
--- Assets/Scripts/utils/PushableCrate.cs
+++ Assets/Scripts/utils/PushableCrate.cs
@@ -4,14 +4,26 @@ using UnityEngine;
 
 public class PushableCrate : MonoBehaviour
 {
+    public float Weight { get; private set; } = 5f;
+    public bool OnALift { get; set; }
+
+    [Header ("pushing parameters")]
     [SerializeField] private float pushSpeedModifier = 1f;
     [SerializeField] private float cratePushSmoothTime = 0.8f;
+
+    [Header("Externals")]
+    [SerializeField] private CrateLift crateLift;
+
     private Rigidbody2D crateRb;
     private float crateVelRef = 0f;
 
     private void Awake()
     {
         crateRb = GetComponent<Rigidbody2D>();
+        if(crateLift == null)
+        {
+            throw new MissingReferenceException();
+        }
     }
 
     private void OnCollisionStay2D(Collision2D collision)
@@ -21,11 +33,23 @@ public class PushableCrate : MonoBehaviour
             //impart player velocity to this crate with some modifier
             float newXVel = Mathf.SmoothDamp(
                     crateRb.velocity.x,
-                    collision.otherRigidbody.velocity.x,
+                    collision.otherRigidbody.velocity.x * pushSpeedModifier,
                     ref crateVelRef,
                     cratePushSmoothTime
             );
             crateRb.velocity = new Vector2(newXVel, crateRb.velocity.y);
+        }
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        //if its another box, we update the global weight list for the lift
+        if (collision.gameObject.CompareTag("dungeon2_pushableCrate"))
+        {
+            if (!collision.gameObject.GetComponent<PushableCrate>().OnALift) return;
+            crateLift.NetLiftWeight += Weight;
+            OnALift = true;
+            crateLift.CheckAndUpdateMapIndex();
         }
     }
 
@@ -35,6 +59,14 @@ public class PushableCrate : MonoBehaviour
         {
             //remove all velocity from this box:
             crateRb.velocity = Vector2.zero;
+        }
+
+        if (collision.gameObject.CompareTag("dungeon2_pushableCrate"))
+        {
+            if (!collision.gameObject.GetComponent<PushableCrate>().OnALift) return;
+            crateLift.NetLiftWeight -= Weight;
+            OnALift = false;
+            crateLift.CheckAndUpdateMapIndex();
         }
     }
 }
